c66a6cbdeb7a60299a8547e2c264bbd3c84719ab
ASproson <a.sproson@outlook.com>
2024-07-22 14:16:37 +0100

    Notes

--- src/arraysAndHashing.ts
+++ src/arraysAndHashing.ts
@@ -1,7 +1,10 @@
+// npm run test:arrayHash
+
 /**
  * @todo Determine whether or not the passed nums array has duplicates in O(n) time
  * @param nums array of numbers
  * @returns boolean based on whether or not passed array contains duplicates
+ * @satisfies O(n) time by traversing the passed array only once
  */
 export const hasDuplicate = (nums: number[]): boolean => {
   return false;
