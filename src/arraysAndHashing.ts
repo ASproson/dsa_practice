b56290a28a9d0233fc077f750c6ab49043ccd431
ASproson <a.sproson@outlook.com>
2024-07-22 14:06:33 +0100

    Adds challenge question

--- src/arraysAndHashing.ts
+++ src/arraysAndHashing.ts
@@ -1,3 +1,8 @@
+/**
+ * @todo Determine whether or not the passed nums array has duplicates in O(n) time
+ * @param nums array of numbers
+ * @returns boolean based on whether or not passed array contains duplicates
+ */
 export const hasDuplicate = (nums: number[]): boolean => {
   return false;
 };
