478c263f26af4a4d35efb5f9bfbb357da9443159
ASproson <a.sproson@outlook.com>
2024-07-22 14:14:34 +0100

    Adds solution comments

--- src/arraysAndHashing.test.ts
+++ src/arraysAndHashing.test.ts
@@ -3,6 +3,7 @@ import { hasDuplicate } from './arraysAndHashing';
 describe('hasDuplicate()', () => {
   const n1 = [1, 2, 3, 3];
   const n2 = [1, 2, 3, 4];
+  const n3 = [100];
   it('returns type boolean', () => {
     expect(typeof hasDuplicate(n1)).toEqual('boolean');
   }),
@@ -11,5 +12,8 @@ describe('hasDuplicate()', () => {
     }),
     it('returns false when given array with duplicates', () => {
       expect(hasDuplicate(n2)).toEqual(false);
+    }),
+    it('returns false when given an array length of 1', () => {
+      expect(hasDuplicate(n3)).toEqual(false);
     });
 });
--- src/arraysAndHashing.ts
+++ src/arraysAndHashing.ts
@@ -1,8 +1,21 @@
+// npm run test:arrayHash
+
 /**
  * @todo Determine whether or not the passed nums array has duplicates in O(n) time
  * @param nums array of numbers
  * @returns boolean based on whether or not passed array contains duplicates
+ * @satisfies O(n) time as traverse the passed array a maximum of one times in the worst case scenario. O(n) space as we potentially store an entirely unique array in the set
  */
 export const hasDuplicate = (nums: number[]): boolean => {
+  if (nums.length <= 1) return false;
+
+  const set = new Set();
+  for (const num of nums) {
+    // O(1) lookup to set
+    if (set.has(num)) return true;
+    // O(1) adding to set
+    set.add(num);
+  }
+
   return false;
 };
