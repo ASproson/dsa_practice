3a7edc6be83af8c1866d0686f0300f010a332581
ASproson <a.sproson@outlook.com>
2024-07-22 14:37:04 +0100

    Adds base

--- src/arraysAndHashing.ts
+++ src/arraysAndHashing.ts
@@ -19,3 +19,14 @@ export const hasDuplicate = (nums: number[]): boolean => {
 
   return false;
 };
+
+/**
+ * @todo Determine whether or not the passed strings are anagrams
+ * @param s string
+ * @param t string
+ * @returns boolean based on whether or not passed strings are anagrams
+ * @satisfies O(n) time and O(n) space
+ */
+export const isAnagram = (s: string, t: string): boolean => {
+  return false;
+};
