31b64c15af7332dca944541e76b467be71ef12a1
ASproson <a.sproson@outlook.com>
2024-07-22 14:22:52 +0100

    Pulls from main

--- src/arraysAndHashing.test.ts
+++ src/arraysAndHashing.test.ts
@@ -1,19 +1,20 @@
 import { hasDuplicate } from './arraysAndHashing';
 
+/**
+ * Select specific describe blocks to run with describe.only
+ * Select specific it blocks to run with it.only
+ */
+
 describe('hasDuplicate()', () => {
   const n1 = [1, 2, 3, 3];
   const n2 = [1, 2, 3, 4];
-  const n3 = [100];
   it('returns type boolean', () => {
     expect(typeof hasDuplicate(n1)).toEqual('boolean');
-  }),
-    it('returns true when given array with duplicates', () => {
-      expect(hasDuplicate(n1)).toEqual(true);
-    }),
-    it('returns false when given array with duplicates', () => {
-      expect(hasDuplicate(n2)).toEqual(false);
-    }),
-    it('returns false when given an array length of 1', () => {
-      expect(hasDuplicate(n3)).toEqual(false);
-    });
+  });
+  it('returns true when given array with duplicates', () => {
+    expect(hasDuplicate(n1)).toEqual(true);
+  });
+  it('returns false when given array with duplicates', () => {
+    expect(hasDuplicate(n2)).toEqual(false);
+  });
 });
