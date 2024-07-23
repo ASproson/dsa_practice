48e403f9eacc7e8b7432f3539c8355a2fc3f0826
ASproson <a.sproson@outlook.com>
2024-07-23 13:37:48 +0100

    Adds initial linkedLists

--- src/linkedLists.ts
+++ src/linkedLists.ts
@@ -2,3 +2,29 @@
 // Be sure to remove .skip from relevant describe blocks in linkedList.test.ts
 
 // git diff main..solutions src/linkedList.ts
+
+/**
+ * Definition for a singly-linked list
+ */
+export type ListNodeOrNull = ListNode | null;
+export class ListNode {
+  val: number;
+  next: ListNodeOrNull;
+  constructor(val = 0, next = null) {
+    (this.val = val), (this.next = next);
+  }
+}
+
+export const reverseList = (head: ListNodeOrNull): ListNodeOrNull => {
+  let curr: ListNodeOrNull = head;
+  let prev: ListNodeOrNull = null;
+
+  while (curr) {
+    const nextNode: ListNodeOrNull = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextNode;
+  }
+
+  return prev;
+};
