95b7a232c0110314786c0b916b5e0156a9292f79
ASproson <a.sproson@outlook.com>
2024-07-23 13:48:40 +0100

    Adds solution comments to reverse list

--- src/linkedLists.ts
+++ src/linkedLists.ts
@@ -15,18 +15,31 @@ export class ListNode {
     }
 }
 
+/**
+ * @todo Reverse the passed linked list (a -> b -> null becomes null -> b -> a)
+ * @param head ListNode | null
+ * @returns reversed linked list
+ * @satisfies O(n) time as we must traverse the entire list, O(1) space as list reversal is done in-place
+ */
 export const reverseList = (head: ListNodeOrNull): ListNodeOrNull => {
-    // We first need to store
+    // If head does not exist, there's nothing to reverse
+    if (!head) return head;
 
+    // Initialize two pointers: curr to traverse the list and prev to track the reversed portion
     let curr: ListNodeOrNull = head;
+    // This will represent the new head of the list when it is reversed, think of it as the tail of the list
     let prev: ListNodeOrNull = null;
 
     while (curr) {
+        // Store the next node in the list so that we point curr to prev
+        // null -> a -> b now becomes null <- a | b
         const nextNode: ListNodeOrNull = curr.next;
         curr.next = prev;
+        // Move prev and curr forward one node in the list
         prev = curr;
         curr = nextNode;
     }
 
+    // Once the loop is complete, prev now points to the new head of the reversed list
     return prev;
 };
